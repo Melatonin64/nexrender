479701fb9c2025689c69dbdaf2ca329a4920f5fc
Tomer Lahav <tomer@eko.com>
2023-08-07 14:44:14 +0300

    Add a configurable "skipSteps" setting to allow skipping render steps.

--- packages/nexrender-core/src/helpers/state.js
+++ packages/nexrender-core/src/helpers/state.js
@@ -1,4 +1,8 @@
 module.exports = (job, settings, fn, fnName) => {
+    if (settings && settings.skipSteps && settings.skipSteps.includes(fnName)) {
+        return Promise.resolve(job);
+    }
+
     job.state = `render:${fnName}`;
 
     if (job.onChange) {
