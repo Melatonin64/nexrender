9403d51c0fc5368c31d46c9010f8308d54c3f080
Tomer Lahav <tomer@eko.com>
2023-12-02 20:20:32 +0200

    nexrender-core - replace rimraf with fs-extra for cleanup (fixes EPERM issues on Windows).

--- packages/nexrender-core/src/tasks/cleanup.js
+++ packages/nexrender-core/src/tasks/cleanup.js
@@ -1,10 +1,10 @@
-const rimraf = require('rimraf')
+const fse = require('fs-extra')
 const path = require('path')
 
 /**
  * Clean up all workpath files and remove folder
  */
-module.exports = function(job, settings) {
+module.exports = async function(job, settings) {
     settings.track('Job Cleanup');
 
     if (settings.skipCleanup) {
@@ -12,23 +12,20 @@ module.exports = function(job, settings) {
         return Promise.resolve(job)
     }
 
-    return new Promise((resolve) => {
-        settings.logger.log(`[${job.uid}] cleaning up...`);
+    settings.logger.log(`[${job.uid}] cleaning up...`);
 
-        // sometimes this attribute (workpath) is undefined
-        if (!job.workpath) {
-            job.workpath = path.join(settings.workpath, job.uid)
-        }
+    // sometimes this attribute (workpath) is undefined
+    if (!job.workpath) {
+        job.workpath = path.join(settings.workpath, job.uid)
+    }
 
-        rimraf(job.workpath, {glob: false}, (err) => {
-            if (!err) {
-                settings.logger.log(`[${job.uid}] Temporary AfterEffects project deleted. If you want to inspect it for debugging, use "--skip-cleanup"`)
-            } else {
-                settings.logger.log(`[${job.uid}] Temporary AfterEffects could not be deleted. (Error: ${err.code}). Please delete the folder manually: ${job.workpath}`)
-            }
+    try {
+        await fse.emptyDir(job.workpath);
+        await fse.remove(job.workpath);
+        settings.logger.log(`[${job.uid}] Temporary AfterEffects project deleted. If you want to inspect it for debugging, use "--skip-cleanup"`)
+    } catch (err) {
+        settings.logger.log(`[${job.uid}] Temporary AfterEffects could not be deleted. (Error: ${err.code}). Please delete the folder manually: ${job.workpath}\n${err}`)
+    }
 
-            resolve(job)
-        })
-    })
+    return job;
 };
-
